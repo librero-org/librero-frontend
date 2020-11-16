eec545d4992376eba8353b9d8d8a382d6f499828
Paloma Ramirez Meza <p.ramirez.meza.8080@gmail.com>
2020-11-16 12:09:42 -0600

    Se imprimen valores de input en consola

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+/* variable */
 const menuBtn = document.querySelector(".menu-icon span");
 const searchBtn = document.querySelector(".search-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
 const items = document.querySelector(".nav-items");
 const form = document.querySelector("form");
+
+/* action */
 menuBtn.onclick = () => {
     items.classList.add("active");
     menuBtn.classList.add("hide");
@@ -21,4 +24,14 @@ searchBtn.onclick = () => {
     form.classList.add("active");
     searchBtn.classList.add("hide");
     cancelBtn.classList.add("show");
+}
+/* Form contact */
+const inpName = document.getElementById("name");
+const inpEmail = document.getElementById("email");
+const inpMessage = document.getElementById("message");
+
+function sendEmail() {
+    console.log(inpName.value);
+    console.log(inpEmail.value);
+    console.log(inpMessage.value);
 }
