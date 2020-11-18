e0c3c852dc24af5a0d22fe820cf968895f59d850
Paloma Ramirez Meza <p.ramirez.meza.8080@gmail.com>
2020-11-18 17:05:55 -0600

    Antes de implementar Router

---
+++ src/footer/footer-component.js
@@ -0,0 +1,38 @@
+import './footer-style.css'
+
+export function FooterComponent() {
+    return (
+        <div>
+            <div className="footer-div">
+                <div className="footer-container">
+                    <ul>
+                        <li>
+                            <img src="./librero_logo.svg" alt="" />
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h2>Acerca de</h2>
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, voluptatem? Nostrum quidem cum omnis? Modi temporibus praesentium atque delectus magnam!
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h2>Follow me</h2>
+                        </li>
+                        <li>
+                            <a href="#"><i className="fab fa-facebook"></i></a>
+                            <a href="#"><i className="fab fa-twitter"></i></a>
+                            <a href="#"><i className="fab fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <footer className="footer">
+                <h6>Libreria Copyright <i className="far fa-copyright"></i></h6>
+            </footer>
+        </div>
+    )
+}
