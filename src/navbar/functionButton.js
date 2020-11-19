b7cd8d872403b76b4354bc29db293a8a43b0ea28
Paloma Ramirez Meza <p.ramirez.meza.8080@gmail.com>
2020-11-19 17:40:18 -0600

    Arreglado, se oculta menu en modo mobile cuando se da clic en un enlance

--- src/navbar/functionButton.js
+++ src/navbar/functionButton.js
@@ -39,3 +39,19 @@ export function menuButton() {
     searchBtn.classList.add("hide");
     cancelBtn.classList.add("show");
 }
+export function closeNavBar(){
+    /* variable */
+    const menuBtn = document.querySelector(".menu-icon span");
+    const searchBtn = document.querySelector(".search-icon");
+    const cancelBtn = document.querySelector(".cancel-icon");
+    const items = document.querySelector(".nav-items");
+    const form = document.querySelector("form");
+
+    /* Actions */
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    form.classList.remove("active");
+    cancelBtn.style.color = "#ff3d00";
+}
--- src/navbar/navbar-component.js
+++ src/navbar/navbar-component.js
@@ -1,28 +1,43 @@
 import './navbar-style.css';
 import { Link } from 'react-router-dom';
-import { searchButton, cancelButton, menuButton } from './functionButton';
+import { searchButton, cancelButton, menuButton, closeNavBar } from './functionButton';
 
 export function NavbarComponent() {
     return (
         <nav className="navbar">
             <div id="menu-icono" className="menu-icon" onClick={menuButton}>
-                <span className="fas fa-bars"></span>
+                <span className="fas fa-bars" />
             </div>
             <img src="../img/librero_logo.svg" alt="" />
             <ul className="nav-items">
-                <li><Link to="./"><span className="fas fa-home"></span>Home</Link></li>
-                <li><Link to=""><span className="fas fa-book"></span>Libros</Link></li>
-                <li><Link to=""><span className="fas fa-clipboard"></span>Pedidos</Link></li>
-                <li><Link to="./contact"><span className="fas fa-address-card"></span>Contacto</Link></li>
+                <li>
+                    <Link to="./" onClick={closeNavBar}>
+                        <span className="fas fa-home" />Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="" onClick={closeNavBar}>
+                        <span className="fas fa-book" />Libros
+                    </Link>
+                </li>
+                <li>
+                    <Link to="" onClick={closeNavBar}>
+                        <span className="fas fa-clipboard" />Pedidos
+                    </Link>
+                </li>
+                <li>
+                    <Link to="./contact" onClick={closeNavBar}>
+                        <span className="fas fa-address-card" />Contacto</Link>
+                </li>
             </ul>
             <div className="search-icon" onClick={searchButton}>
-                <span className="fas fa-search"></span>
+                <span className="fas fa-search" />
             </div>
             <div className="cancel-icon" onClick={cancelButton}>
-                <span className="fas fa-times"></span>
+                <span className="fas fa-times" />
             </div>
             <form action="#">
-                <input type="search" className="search-data" placeholder="Search" required></input>
+                <input type="search" className="search-data" placeholder="Search" required />
                 <button type="submit" className="fas fa-search"></button>
             </form>
         </nav>
