5257454e46a8c1a6cb72600ea00bae2816dd3931
Paloma Ramirez Meza <p.ramirez.meza.8080@gmail.com>
2020-11-26 12:30:29 -0600

    Cambio de clase a funcion y usando useMutation

--- src/contact/contact-component.js
+++ src/contact/contact-component.js
@@ -1,42 +1,51 @@
 import './contact-style.css';
 import React from 'react';
-import { LoaderComponent } from '../loader/loader-component';
-import { ScreenErrorComponent } from '../screen-error/screen-error-component';
-import { useQuery, gql } from "@apollo/client";
-import { ContactFormComponent } from '../contact-form/contact-form-component';
+import { useMutation, gql } from "@apollo/client";
 
-
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(gql`
-     {
-       hello
-     }
-   `);
-
-  if (loading) {
-    return (
-      <LoaderComponent></LoaderComponent>
-    );
+const SEND_CONTACT_EMAIL = gql`
+  mutation($data: SendContactEmailInput!) {
+    sendContactEmail(data: $data)
   }
-
-  if (error) {
-    return (
-      <ScreenErrorComponent></ScreenErrorComponent>
-    );
-  }
-  if (data) {
-    console.log(data.hello);
-    return (
-      <div>
-        <p>{data.hello}</p>
-        <ContactFormComponent />
-      </div>
-    );
-  }
-}
+`;
 
 export function ContactComponent() {
+  const [mutate, data] = useMutation(SEND_CONTACT_EMAIL);
+  let name, email, message;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    mutate({
+      variables: {
+        data: {
+          fromEmailAddress: email.value,
+          fromName: name.value,
+          message: message.value
+        }
+      }
+    });
+  }
+
+  if (data) console.log(data);
   return (
-    <ExchangeRates />
+    <div>
+      <h2>Contáctanos</h2>
+      <p>Si tiene alguna duda o comentario, puede ponerse en contacto con nosotros.</p>
+      <br />
+      <div className="contact-container">
+        <form className="form-contact" onSubmit={handleSubmit}>
+          <p>Por favor, llena los campos requeridos.</p>
+          <h4>Nombre: *</h4>
+          <input type="text" name="name" ref={(node => name = node)} required />
+          <span className="fas fa-address-card icon_input"></span>
+          <h4>E-mail: </h4>
+          <input type="text" name="email" ref={(node => email = node)} required />
+          <span className="fas fa-envelope-open-text icon_input"></span>
+          <h4>Mensaje: *</h4>
+          <textarea name="message" rows="4" cols="50" ref={(node => message = node)} required />
+          <button type="submit" className="fas fa-search"> Enviar</button>
+        </form>
+      </div>
+    </div>
   );
 }
