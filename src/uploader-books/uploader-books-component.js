903486561ca461ecbe67d3618ae2d6e2a51849e3
Paloma Ramirez Meza <p.ramirez.meza.8080@gmail.com>
2020-12-03 11:12:34 -0600

    UploadBook funcionando, v1

--- src/uploader-books/uploader-books-component.js
+++ src/uploader-books/uploader-books-component.js
@@ -1,26 +1,58 @@
-import './uploader-books-style.css';
-import { functionTagInputFile, hola } from './functionTagInputFile';
+import "./uploader-books-style.css";
+// import { functionTagInputFile } from "./functionTagInputFile";
+import { gql, useMutation } from "@apollo/client";
+
+const UPLOAD_BOOKS = gql`
+  mutation($data: UploadBookInput!) {
+    uploadBook(data: $data) {
+      id
+      title
+      authors
+      url
+      coverUrl
+    }
+  }
+`;
+
+function ExchangeRates() {
+    let title, author, file;
+    const [mutate, { data, error }] = useMutation(UPLOAD_BOOKS);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Titulo: " + title.value + "\nAutor: " + author.value + '\nFile: ' + file);
+        mutate({
+            variables: {
+                data: {
+                    book: { title: title.value, authors: [author.value] },
+                    file: file.files[0],
+                },
+            },
+        });
+    };
+
+    return (
+        <div>
+            <div className="form-small-container">
+            <form className="form-small" onSubmit={handleSubmit}>
+            <h4>Titulo del libro: *</h4>
+                <input type="text" name="title" pattern="[A-Za-z]{1,40}" ref={(node) => (title = node)} required/>
+                <span className="fas fa-book-open icon_input"></span>
+                <h4>Autor(es): *</h4>
+                <input type="text" name="author" pattern="[A-Za-z]{1,40}" ref={(node) => (author = node)} required/>
+                <span className="fas fa-address-card icon_input"></span>
+                <input type="file" name="file" ref={(node) => (file = node)} />
+                <button type="submit" className="fas fa-search"> Enviar </button>
+            </form>
+        </div>
+        </div>
+    );
+}
 
 export function UploaderBooksComponent() {
     return (
         <div>
-            <div className="form-small-container">
-                <form className="form-small">
-                    <h4>Titulo del libro: *</h4>
-                    <input type="text" name="book-title" pattern="[A-Za-z]{1,40}" title="Solo letras mayúsculas o minúsculas" maxLength="40" required />
-                    <span className="fas fa-address-card icon_input"></span>
-                    <h4>Autor(es): *</h4>
-                    <input type="text" name="book-author" pattern="[A-Za-z]{1,40}" title="Solo letras mayúsculas o minúsculas" maxLength="40" required />
-                    <span className="fas fa-address-card icon_input"></span>
-                    <h4>Busca tu archivo: *</h4>
-                    <span className="fas fa-upload icon_input2"></span>
-                    <input type="file" id="input-file" />
-                    <label for="input-file" onClick={functionTagInputFile}>Buscar</label>{/* Boton del input file */}
-                    <span id="input-file-name" className="text">Nada</span>
-                    <br /><br />
-                    <button type="submit" className="fas fa-search"> Enviar</button>
-                </form>
-            </div>
+            <ExchangeRates />
         </div>
     );
 }
